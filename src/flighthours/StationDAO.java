8cd05b542adb53d4f33a96bb4f2a3c2d0c3433d6
tamerjj1 <tamerjj1@TAMERJJ1-WL6.dom1.jhuapl.edu>
2018-08-31 10:06:34 -0400

    Class to create and populate missions table

--- src/flighthours/StationDAO.java
+++ src/flighthours/StationDAO.java
@@ -5,10 +5,6 @@
  */
 package flighthours;
 
-/**
- *
- * @author tamerjj1
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -28,9 +24,10 @@ public class StationDAO {
     private static Connection conn = null;
     private static Statement stmt = null;
     private static PreparedStatement selectStationByType;
+    private static PreparedStatement selectAllStations;
     private static List<Station> stationList;
 
-   /* public static void main(String[] args) {
+    /* public static void main(String[] args) {
         createConnection();
         //selectAllStations();
         //selectStationType("AMO");
@@ -38,55 +35,47 @@ public class StationDAO {
         System.out.println(stationList.size());
         shutdown();
      } */
-
     public StationDAO() {
         try {
-            
+
             conn = DriverManager.getConnection(dbURL);
             selectStationByType = conn.prepareStatement("SELECT * FROM STATIONS WHERE STATION_TYPE = ?");
+            selectAllStations = conn.prepareStatement("SELECT * FROM STATIONS");
 
         } catch (Exception except) {
             except.printStackTrace();
         }
-        
+
         //createConnection();
     }
-    private static void createConnection() {
-        try {
-            
-            conn = DriverManager.getConnection(dbURL);
-            selectStationByType = conn.prepareStatement("SELECT * FROM STATIONS WHERE STATION_TYPE = ?");
 
-        } catch (Exception except) {
-            except.printStackTrace();
-        }
-    }
+    public static List< Station> selectAllStations() {
 
-    private static void selectAllStations() {
+        List<Station> results = null;
+        ResultSet resultSet = null;
 
         try {
-            stmt = conn.createStatement();
-            ResultSet results = stmt.executeQuery("select * from " + tableName);
-            ResultSetMetaData rsmd = results.getMetaData();
-            int numberCols = rsmd.getColumnCount();
-            for (int i = 1; i <= numberCols; i++) {
-                //print Column Names
-                System.out.print(rsmd.getColumnLabel(i) + "\t\t");
+            resultSet = selectAllStations.executeQuery();
+            results = new ArrayList<Station>();
+
+            while (resultSet.next()) {
+                results.add(new Station(
+                        resultSet.getInt("STATION_ID"),
+                        resultSet.getString("STATION_NAME"),
+                        resultSet.getString("STATION_TYPE")));
             }
 
-            System.out.println("\n-------------------------------------------------");
-
-            while (results.next()) {
-                int id = results.getInt(1);
-                String restName = results.getString(2);
-                String cityName = results.getString(3);
-                System.out.println(id + "\t\t" + restName + "\t\t" + cityName);
-            }
-            results.close();
-            stmt.close();
         } catch (SQLException sqlExcept) {
             sqlExcept.printStackTrace();
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException sqlExcept) {
+                sqlExcept.printStackTrace();
+            }
         }
+
+        return results;
     }
 
     public static List< Station> selectStationByType(String stationType) {
@@ -133,3 +122,4 @@ public class StationDAO {
     }
 
 }
+
