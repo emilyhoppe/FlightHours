4a169b143a8e47aa4ddb14c3588935acc1b9c2a6
tamerjj1 <tamerjj1@TAMERJJ1-WL6.dom1.jhuapl.edu>
2018-09-12 15:44:39 -0400

    Initial commit

---
+++ src/flighthours/Mission.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flighthours;
+
+public class Mission {
+    
+    private int missionID;
+    private String missionName;
+
+    public Mission() {
+    }
+
+    public Mission(int missionID, String missionName) {
+        this.missionID = missionID;
+        this.missionName = missionName;
+    }
+
+    public int getMissionID() {
+        return missionID;
+    }
+
+    public void setMissionID(int missionID) {
+        this.missionID = missionID;
+    }
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public void setMissionName(String missionName) {
+        this.missionName = missionName;
+    }
+
+    @Override
+    public String toString() {
+        return "Mission{" + "missionID=" + missionID + ", missionName=" + missionName + '}';
+    }
+    
+    
+    
+}
