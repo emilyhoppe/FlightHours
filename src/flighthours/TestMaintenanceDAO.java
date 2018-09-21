5bdfce5fa48b920f01eb2a321ceb15f2aef09a82
JGRIMARD <none>
2018-09-21 19:26:09 -0400

    Revised Date(String) because it was causing compile warnings.

--- src/flighthours/TestMaintenanceDAO.java
+++ src/flighthours/TestMaintenanceDAO.java
@@ -9,16 +9,20 @@ package flighthours;
  *
  * @author ehoppe
  */
-
 import controller.MaintenanceDAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class TestMaintenanceDAO {
+
     private static String dbURL = "jdbc:derby:FlightHours";
     private static String tableName = "AIRCRAFT";
 
@@ -34,26 +38,37 @@ public class TestMaintenanceDAO {
 
         } catch (Exception except) {
             except.printStackTrace();
-        }        
-        
+        }
+
         mtQueries = new MaintenanceDAO();
         int aircraftID = 4;
         mtTableModel = mtQueries.selectMaintenanceByAircraft(aircraftID);
-        
+
         //Section below is to test and print results
         int rowCount = mtTableModel.getRowCount();
-        int columnCount = mtTableModel.getColumnCount();  
+        int columnCount = mtTableModel.getColumnCount();
         for (int row = 0; row < rowCount; row++) {
             for (int column = 0; column < columnCount; column++) {
-                System.out.print(mtTableModel.getValueAt(row, column) + ", ");                 
+                System.out.print(mtTableModel.getValueAt(row, column) + ", ");
             }
             System.out.println();
-        } 
-        
+        }
+
         //New Maintenance Test Verified
         int maintenanceID = 1;
-        Date start = new Date("5/10/2018");
-        Date end = new Date("5/11/2018");
+        //Date(String is giving the compile warning: 
+        //warning: [deprecation] Date(String) in Date has been deprecated
+        //Changing to use SimpleDateFormat instead - JG
+        //Date start = new Date("5/10/2018");
+        //Date end = new Date("5/11/2018");
+        Date start = null;
+        Date end = null;
+        try {
+            start = new SimpleDateFormat("MM/dd/yyyy").parse("5/10/2018");
+            end = new SimpleDateFormat("MM/dd/yyyy").parse("5/10/2018");
+        } catch (ParseException ex) {
+        }
+
         //System.out.println(start.toString());
         String desc = "testing";
         Maintenance inMaintenance = new Maintenance();
@@ -62,13 +77,11 @@ public class TestMaintenanceDAO {
         inMaintenance.setStartDate(start);
         inMaintenance.setEndDate(end);
         inMaintenance.setMaintDescr(desc);
-        
+
         //int success = mtQueries.insertNewMaintenance(inMaintenance);
         //System.out.println(String.valueOf(success));
-        
         //int success = mtQueries.modifyMaintenance(inMaintenance);
         //System.out.println(String.valueOf(success));
-        
         System.out.println("Stop Here");
     }
 
--- src/flighthours/TestOperationDAO.java
+++ src/flighthours/TestOperationDAO.java
@@ -9,14 +9,16 @@ package flighthours;
  *
  * @author ehoppe
  */
-
 import controller.OperationDAO;
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class TestOperationDAO {
+
     private static String dbURL = "jdbc:derby:FlightHours";
     private static String tableName = "AIRCRAFT";
 
@@ -26,25 +28,36 @@ public class TestOperationDAO {
     private static DefaultTableModel opTableModel;
 
     public static void main(String args[]) {
-        
+
         opQueries = new OperationDAO();
         int aircraftID = 1;
         opTableModel = opQueries.selectOperationsByAircraft(aircraftID);
-        
+
         //Section below is to test and print results
         int rowCount = opTableModel.getRowCount();
-        int columnCount = opTableModel.getColumnCount();  
+        int columnCount = opTableModel.getColumnCount();
         for (int row = 0; row < rowCount; row++) {
             for (int column = 0; column < columnCount; column++) {
-                System.out.print(opTableModel.getValueAt(row, column) + ", ");                 
+                System.out.print(opTableModel.getValueAt(row, column) + ", ");
             }
             System.out.println();
-        } 
-        
+        }
+
         //New Maintenance Test Verified
         int operationID = 1;
-        Date start = new Date("5/10/2018");
-        Date end = new Date("5/11/2018");
+        //Date(String is giving the compile warning: 
+        //warning: [deprecation] Date(String) in Date has been deprecated
+        //Changing to use SimpleDateFormat instead - JG
+        //Date start = new Date("5/10/2018");
+        //Date end = new Date("5/11/2018");
+        Date start = null;
+        Date end = null;
+        try {
+            start = new SimpleDateFormat("MM/dd/yyyy").parse("5/10/2018");
+            end = new SimpleDateFormat("MM/dd/yyyy").parse("5/10/2018");
+        } catch (ParseException ex) {
+        }
+
         //System.out.println(start.toString());
         String desc = "testing";
         Operation inOperation = new Operation();
@@ -56,7 +69,7 @@ public class TestOperationDAO {
         inOperation.setOperationStartDate(start);
         inOperation.setOperationEndDate(end);
         inOperation.setOperationFlightHour(60);
-        
+
         //Insert Operation Test
         /*int success = opQueries.insertNewOperation(inOperation);
         int success1 = opQueries.insertOpAdjustCurrentHours(inOperation);
@@ -64,12 +77,9 @@ public class TestOperationDAO {
         System.out.println(String.valueOf(success));
         System.out.println(String.valueOf(success1));
         System.out.println(String.valueOf(success2));*/
-        
         //Modify Operation Test
         //int success = opQueries.modifyOperation(inOperation);
         //System.out.println(String.valueOf(success));
-        
-        
         System.out.println("Stop Here");
     }
 
